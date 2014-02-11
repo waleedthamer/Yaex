5e8f32ebb192247116dbd6a474c2ce4343bfed26
Xeriab Nabil <xeriab@mefso-exen.localdomain>
2014-02-11 23:59:23 +0300

    Fixing Jakefile.js and removing .node_modules folder

--- Jakefile.js
+++ Jakefile.js
@@ -1,24 +1,25 @@
-/*
-Yaex building, testing and linting scripts.
-
-To use, install Node, then run the following commands in the project root:
-
-    npm install -g jake
-    npm install
-
-To check the code for errors and build Yaex from source, run "jake".
-To run the tests, run "jake test".
-
-For a custom build, open build/build.html in the browser and follow the instructions.
-*/
+/**
+ * Yaex building, testing and linting scripts.
+ *
+ * To use, install Node, then run the following commands in the project root:
+ * 		
+ * 		npm install -g jake
+ * 		npm install
+ *
+ * To check the code for errors and build Yaex from source, run "jake".
+ * To run the tests, run "jake test".
+ *
+ * For a custom build, open build/build.html in the browser and follow the instructions.
+ */
 
 var build = require('./build/build.js');
 
 function hint(msg, paths) {
 	return function () {
 		console.log(msg);
-		jake.exec('node node_modules/jshint/bin/jshint -c ' + paths,
-				{printStdout: true}, function () {
+		jake.exec('node node_modules/jshint/bin/jshint -c ' + paths, {
+			printStdout: true
+		}, function () {
 			console.log('\tCheck passed.\n');
 			complete();
 		});
@@ -26,18 +27,24 @@ function hint(msg, paths) {
 }
 
 desc('Check Yaex source for errors with JSHint');
-task('lint', {async: true}, hint('Checking for JS errors...', 'build/hintrc.js src'));
 
-desc('Check Yaex specs source for errors with JSHint');
-task('lintspec', {async: true}, hint('Checking for specs JS errors...', 'spec/spec.hintrc.js spec/suites'));
+task('lint', {
+	async: true
+}, hint('Checking for JS errors...', 'build/hintrc.js src'));
 
 desc('Combine and compress Yaex source files');
-task('build', {async: true}, function (compsBase32, buildName) {
+
+task('build', {
+	async: true
+}, function (compsBase32, buildName) {
 	build.build(complete, compsBase32, buildName);
 });
 
 desc('Run PhantomJS tests');
-task('test', ['lint', 'lintspec'], {async: true}, function () {
+
+task('test', ['lint', 'lintspec'], {
+	async: true
+}, function () {
 	build.test(complete);
 });
 
